20b310a0348920082476aaf41aadaa32d562a793
Samyuktha Venkatesan <samyu.vj97@gmail.com>
2024-05-29 20:15:29 -0700

    Search bar backend and frontend changes

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 //app.use("/api/users",userRoutes)
 
 app.use('/api/auth', authRoutes);
-app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes);
 
 
 app.get("/",(req,res) =>{
