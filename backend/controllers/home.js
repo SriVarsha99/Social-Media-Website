349a2179f5ad68d16a288f07044b4a0ea81ddcec
simrdhaw <simrandhawan2110@gmail.com>
2024-05-31 02:20:19 -0700

    like count

--- backend/controllers/home.js
+++ backend/controllers/home.js
@@ -21,4 +21,14 @@ export const comments =(req,res)=>{
     });
   }
 
+  export const likes =(req,res)=>{
+    const q = "select post_id, like_count from posts where post_id = ?;";
+    // Get the username from request headers!
+    db.query(q, [req.headers.post_id], (err, data) => {
+      if (err) return res.status(500).json({ message: "Internal server error", error: err });
+      //console.log(data);
+      res.status(200).json(data);
+    });
+  }
+
   // add routes to insert to tables later.
--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {posts, comments} from "../controllers/home.js"
+import {posts, comments, likes} from "../controllers/home.js"
 const router = express.Router()
 router.get("/posts", posts)
 router.get("/comments", comments)
+router.get("/likes", likes)
 
 export default router
