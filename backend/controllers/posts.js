86ef36c3d7eee8a4f7be254ba09972d727311310
Simran Dhawan <simrandhawan2110@gmail.com>
2024-05-31 15:35:52 -0700

    Commiting comment likes changes

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,8 +1,9 @@
 import { db } from "../connect.js";
+import moment from "moment";
 
 export const posts =(req,res)=>{
   
-  const q = "select post_id, user_id, content as desc, post_time, like_count, comment_count from Posts where user_id = ? order by post_time desc;";
+  const q = "select post_id, user_id, content , post_time, like_count, comment_count from Posts where user_id = ? order by post_time desc;";
 
   // Get the username from request headers!
   db.query(q, [req.headers.username], (err, data) => {
@@ -30,5 +31,33 @@ export const comments =(req,res)=>{
       res.status(200).json(data);
     });
   }
+
+  export const addLike = (req,res)=>{
+    const q = "INSERT INTO likes (`user_id`,`post_id`) VALUES (?);";
+    const values = [
+      req.headers.user_id,
+      req.headers.post_id
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+        return res.status(200).json("Post has been liked.");
+      });
+    };
+
+    export const addComment = (req, res) => {
+      const q = "INSERT INTO comments(`text`, `datetime`, `user_id`, `post_id`) VALUES (?)";
+      const values = [
+      req.headers.text,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      req.headers.user_id,
+      req.headers.post_id];
+    
+      db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+        return res.status(200).json("Comment has been created.");
+      });
+    };
+
 
   // add routes to insert to tables later.
