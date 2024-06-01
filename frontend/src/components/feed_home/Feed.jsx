d8e644d7fb6a76014e362d986dcde73715fac391
Samyuktha Venkatesan <samyu.vj97@gmail.com>
2024-05-31 18:49:37 -0700

    Committing for home

--- frontend/src/components/feed_home/Feed.jsx
+++ frontend/src/components/feed_home/Feed.jsx
@@ -1,13 +1,42 @@
 import Post from "../postt/Post";
 import "./feed.css";
 import { Posts } from "../../dummyData";
+import { useEffect, useState } from "react";
 
 export default function Feed() {
+
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8800/api/home/posts", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({user_id: 1})
+    })
+        .then((response) => response.json())
+        .then((json) =>{
+          setPosts(json);
+        });
+  }, []);
+
+  
+  useEffect(() => {
+    fetch("http://localhost:8800/api/users/")
+    .then((response) => response.json())
+    .then((json) =>{
+      setUsers(json);
+    });
+}, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
+        {posts.map((p) => (
+          <Post key={p.post_id} post={p} users = {users}/>
         ))}
       </div>
     </div> 
