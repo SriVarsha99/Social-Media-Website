b28a237c1b463c61a38362fbfd4dcc24361efb71
Samyuktha Venkatesan <samyu.vj97@gmail.com>
2024-06-01 12:35:43 -0700

    Making home/like and css changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,7 +9,25 @@ export default function Home() {
       <Topbar />
       <div className="homeContainer">
         <Sidebar />
+        <div className="homeRight">
+          <div className="homeRightTop">
+            <div className="homeCover">
+              <div
+                className="homeCoverImg"
+              />
+              <div
+                className="homeUserImg"
+              />
+            </div>
+            <div className="homeInfo">
+                <h4 className="homeInfoName">Srivarsha</h4>
+                <span className="homeInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="homeRightBottom">
         <Feed/>
+        </div>
+        </div>
       </div>
     </>
   );
